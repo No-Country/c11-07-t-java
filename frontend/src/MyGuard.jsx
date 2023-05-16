49c058d53e5d8bb2934797f76892152415296317
Roman Arenas <roman.ea3232@gmail.com>
2023-05-16 01:34:11 -0300

    Add front structure

---
+++ frontend/src/MyGuard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MyGuard = () => {
+  return (
+    <div>MyGuard</div>
+  )
+}
---
+++ frontend/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { MyGuard } from './MyGuard'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MyGuard/>
+  </React.StrictMode>,
+)
