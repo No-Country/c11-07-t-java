fbcb48b89df5183fa31b7940b4927a03d96af2ca
gabiezzi <geiezzi90@gmail.com>
2023-05-16 16:51:22 -0300

    Add Project structure Add model OnCall , on Call Service , on Call Controller

---
+++ backend/src/main/java/com/nocountry/myguard/service/OnCallService.java
@@ -0,0 +1,17 @@
+package com.nocountry.myguard.service;
+
+import com.nocountry.myguard.model.OnCall;
+
+import java.util.List;
+
+public interface OnCallService {
+
+    OnCall save(OnCall onCall);
+    OnCall update(Long id, OnCall onCall) throws Exception;
+
+    OnCall findById(Long id) throws Exception;
+
+    List<OnCall> findAll();
+
+    void Delete(Long id) throws Exception;
+}
