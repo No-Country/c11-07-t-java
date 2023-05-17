d3f391739af5804ea728b9c1c876d8cb1ade2139
juan marcos kruppa <87148345+JMK1988@users.noreply.github.com>
2023-05-17 18:20:51 -0300

    FactoryCalendario

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { MyGuard } from './MyGuard'
 
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <MyGuard/>
