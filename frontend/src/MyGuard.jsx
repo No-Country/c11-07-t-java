adc6bd5ec9ec0c4a60b1ad7bca1b8498cbd89ff7
Jafet Vazquez <jetvax.jv@gmail.com>
2023-05-17 21:40:09 -0600

    sidebar, falta asignar mejores estilos.

--- frontend/src/MyGuard.jsx
+++ frontend/src/MyGuard.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import { Footer, Home } from "./components/ui";
+import MySideNav from "./components/ui/SideNav";
 
 export const MyGuard = () => {
   return (
     <BrowserRouter>
       <Home />
       <Footer />
+      <MySideNav />
     </BrowserRouter>
   );
 };
