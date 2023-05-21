ead18bf3977f276b90171a3c5ac9b8bbec700ec5
Daniel Matos <deme12.1@gmail.com>
2023-05-20 21:10:29 -0500

    Created methods to get claims and username from the token, in JwtService

---
+++ backend/src/main/java/com/nocountry/myguard/service/security/JwtService.java
@@ -0,0 +1,34 @@
+package com.nocountry.myguard.service.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.function.Function;
+
+public class JwtService {
+    private static final String SECRET_KEY = "614E645267556B58703273357638792F423F4528472B4B6250655368566D5971";
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
