0837023227d4fdb2acdd98c10d9899eb91feb213
Daniel Matos <deme12.1@gmail.com>
2023-05-22 06:31:12 -0500

    Added missing @Service anotation to JwtService

--- backend/src/main/java/com/nocountry/myguard/service/security/JwtService.java
+++ backend/src/main/java/com/nocountry/myguard/service/security/JwtService.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
@@ -13,6 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+@Service
 public class JwtService {
     private static final String SECRET_KEY = "614E645267556B58703273357638792F423F4528472B4B6250655368566D5971";
     public String extractUsername(String token){
