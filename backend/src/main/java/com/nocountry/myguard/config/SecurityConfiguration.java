d3ce6a72dfb00e2765ee393e7a0b3a737ace5ee5
Daniel Matos <deme12.1@gmail.com>
2023-05-21 20:16:53 -0500

    Added api/auth/** in requestMatchers

--- backend/src/main/java/com/nocountry/myguard/config/SecurityConfiguration.java
+++ backend/src/main/java/com/nocountry/myguard/config/SecurityConfiguration.java
@@ -23,7 +23,7 @@ public class SecurityConfiguration {
             .csrf()
             .disable()
             .authorizeHttpRequests()
-            .requestMatchers("")
+            .requestMatchers("api/auth/**")
             .permitAll()
             .anyRequest()
                 .authenticated()
