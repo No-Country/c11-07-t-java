996638d5c141e35b8dc0f661e6b443b5dfebfae8
Roman Arenas <roman.ea3232@gmail.com>
2023-05-30 17:11:26 -0300

    protected-routes

---
+++ frontend/src/components/ui/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Logo } from "../util";
+import './header.css';
+
+export const Loading = () => {
+  return (
+    <div className="loading">
+      <Logo />
+    </div>
+  );
+};
