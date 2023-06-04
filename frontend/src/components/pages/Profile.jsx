2c70168b51c2688de36ce276c873b810c9e51af4
Roman Arenas <roman.ea3232@gmail.com>
2023-06-04 16:00:27 -0300

    fix-logout-bug

--- frontend/src/components/pages/Profile.jsx
+++ frontend/src/components/pages/Profile.jsx
@@ -10,16 +10,21 @@ export const Profile = () => {
 
 
   useEffect(()=>{
-    axios.get(`http://localhost:8080/api/users/1?id=1`)
+    axios.get(`http://localhost:8080/api/users` )
     .then(data => setProfileData(data))
   }, [])
 
 
+  console.log(profileData)
 
   return (
     <>
       <Header/>
       <SideNabvar/>
+      <div>
+        <h3>Por favor actualize su perfil</h3>
+      
+      </div>
     </>
   )
 }
