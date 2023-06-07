a2cc525817030bb56da69c642cf23d795d3af35b
Roman Arenas <roman.ea3232@gmail.com>
2023-06-07 13:29:41 -0300

    correccion del acceso indebido

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { Calendario, SelectedGuard, Profile } from "./components/pages";
-import {  Loading } from "./components/ui";
+import { Loading } from "./components/ui";
 import { useAuthStore } from "./hooks";
 import { useEffect } from "react";
 import { LoginForm, SignUpForm } from "./components/auth";
@@ -9,18 +9,14 @@ import { Settings } from "./components/pages/Settings";
 export const AppRoutes = () => {
   const { status, checkAuthToken } = useAuthStore();
 
-
   useEffect(() => {
     checkAuthToken();
   }, []);
 
-//const estado = 'not--authenticated'; //ESTA VARIABLE HAY QUE BORRARLA CUANDO SE TERMINA DE USAR Y REEMPLAZAR TODO POR STATUS
+  //const estado = 'not--authenticated'; //ESTA VARIABLE HAY QUE BORRARLA CUANDO SE TERMINA DE USAR Y REEMPLAZAR TODO POR STATUS
 
   if (status == "checking") {
-    setTimeout(() => {
-       return <Loading />;
-    }, 6000);
-   
+    return <Loading />;
   }
   return (
     <Routes>
@@ -28,7 +24,7 @@ export const AppRoutes = () => {
         <>
           <Route path="/auth/*" element={<LoginForm />} />
           <Route path="/register" element={<SignUpForm />} />
-          <Route path="/*" element={<LoginForm/>} />
+          <Route path="/*" element={<LoginForm />} />
         </>
       ) : (
         <>
@@ -36,7 +32,7 @@ export const AppRoutes = () => {
           <Route path="/selec" element={<SelectedGuard />} />
           <Route path="/*" element={<Calendario />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/settings" element={<Settings/>} />
+          <Route path="/settings" element={<Settings />} />
         </>
       )}
     </Routes>
